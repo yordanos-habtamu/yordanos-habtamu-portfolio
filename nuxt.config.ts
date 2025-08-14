b9f2260700d566c2566a43865279709704f6239a
yordanos habtamu <jordrichh7@gmail.com>
2025-08-15 02:58:44 +0300

    added vercel edit from nitro

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import tailwindcss from "@tailwindcss/vite"
 export default defineNuxtConfig({
+  nitro: {
+    preset: 'vercel'
+  },
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
   css : ['~/assets/css/main.css', '@fortawesome/fontawesome-svg-core/styles.css'],
